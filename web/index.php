4c2cae8589049e558f036b80059489fc9922298d
John DoeYvesCharvis <charvisyves@ymail.com>
2017-05-03 11:17:46 +0200

    first commit

---
+++ web/index.php
@@ -0,0 +1,24 @@
+<?php
+use Core\Auth\DBAuth;
+define('ROOT', dirname(__DIR__));
+require ROOT.'/app/App.php'; // permet l'absolute
+App::load();
+
+if (isset($_GET['p'])) {
+	$page = $_GET['p'];
+}else{
+	$page = "home";
+}
+
+/////////////////////////
+
+ob_start();
+if ($page==='home') {  // Charge centre page (contente)
+	require ROOT.'/pages/index.php';
+}elseif ($page==='403') {
+	require ROOT.'/pages/errors/403.php';
+}elseif ($page==='404') {
+	require ROOT.'/pages/errors/404.php';
+}
+$content = ob_get_clean(); // Le template 
+require ROOT.'/pages/templates/default.php'; 
